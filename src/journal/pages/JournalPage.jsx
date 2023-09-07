c6bf64f48352197da864f781b39fdd7d893ed540
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-09-07 15:05:38 -0300

    Add JournalLayout design

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,17 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        repellendus, ipsa aliquid autem magnam ipsam recusandae optio, quidem
+        assumenda eos sit aspernatur sapiente explicabo facere sed. Accusamus
+        expedita nulla nesciunt?
+      </Typography>
+      {/* NothingSelected */}
+      {/* NoteView */}
+    </JournalLayout>
   );
 };
