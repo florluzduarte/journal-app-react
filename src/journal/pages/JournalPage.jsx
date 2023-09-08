a8ed36692edf57c1b258b1134aea221d9e1a8e5d
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-09-08 12:42:02 -0300

    Add NoteView and ImageGallery components

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,18 +1,12 @@
 // import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelectedView } from "../views";
+import { NoteView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        repellendus, ipsa aliquid autem magnam ipsam recusandae optio, quidem
-        assumenda eos sit aspernatur sapiente explicabo facere sed. Accusamus
-        expedita nulla nesciunt?
-      </Typography> */}
-      <NothingSelectedView />
-      {/* NoteView */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   );
 };
